631f1790bad3c870a4b60a25f7502b99f7bbf4af
Furkan Göregen <furkan.goregen@stagedijital.com>
2025-01-10 14:45:35 +0300

    Deployed website

--- components/ImageGrid.js
+++ components/ImageGrid.js
@@ -1,12 +1,19 @@
-import { collection, onSnapshot, orderBy, query, where } from "firebase/firestore";
+import { collection, deleteDoc, doc, onSnapshot, orderBy, query, where } from "firebase/firestore";
 import { useContext, useEffect, useState } from "react"
 import { db } from "../firebase";
 import { motion } from "framer-motion";
 import { AuthContext } from "../contexts/AuthContext";
+import { Grid, IconButton, ListItem, Typography } from "@mui/material";
+import { Delete } from "@mui/icons-material";
 
 export default function ImageGrid({setSelectedImage}) {
     const [docs,setDocs] = useState(null);
     const {email} = useContext(AuthContext);
+
+    const handleClick=async(id)=>{
+      const ref = doc(db,"resimler",id);
+      const delDoc = await deleteDoc(ref);    
+  }
 
     useEffect(()=>{
         const ref = collection(db,"resimler");
@@ -23,9 +30,26 @@ export default function ImageGrid({setSelectedImage}) {
   return (
     <div className="img-grid">
       {docs && docs.map(doc=>(
-        <motion.div className="img-wrap" key={doc.id} layout whileHover={{opacity:1}} onClick={()=>setSelectedImage(doc.url)}>
+        <div>
+          
+        <motion.div style={{marginTop:10}} className="img-wrap" key={doc.id} layout whileHover={{opacity:1}} onClick={()=>setSelectedImage(doc.url)}>
             <motion.img initial={{opacity:0}} animate={{opacity:1}} src={doc.url} alt="yuklendi"/>
         </motion.div>
+        <Grid
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justify="center"
+            >
+            <IconButton onClick={()=>handleClick(doc.id)} edge="middle" aria-label="delete">
+            <Delete/>
+            </IconButton>
+            </Grid>
+        
+       
+        </div>
+           
     ))}
     </div>
   )
